0a6460e43aecfd1ba1f3dc21c455f5aa63ebf71c
Kai-Chun Lin <kaichun0827@gmail.com>
2023-08-15 07:13:14 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { useState, useRef } from 'react';
-import './style.css';
+import React, { useState, useRef } from "react";
+import "./style.css";
 
 export default function App() {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const emailRef = useRef();
 
   const updateEmailHandler = (event) => {
     //console.log(event.target.value);
     const enteredEmail = emailRef.current.value;
-    console.log(enteredEmail)
+    console.log(enteredEmail);
     setEnteredEmail(event.target.value);
   };
   const submitHandler = (event) => {
     event.preventDefault;
     const enteredEmail = emailRef.current.value;
-    console.log(enteredEmail)
+    // console.log(enteredEmail)
   };
 
   return (
